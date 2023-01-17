cc063f8dfca7060fe71d7f1a0075eda7fc9544e8
Danyil Marchuk <zemero58@gmail.com>
2023-01-17 22:22:22 +0100

    Initial Commit

---
+++ H4XOR NEWS/H4XOR_NEWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NEWSApp.swift
+//  H4XOR NEWS
+//
+//  Created by Данік on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NEWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
